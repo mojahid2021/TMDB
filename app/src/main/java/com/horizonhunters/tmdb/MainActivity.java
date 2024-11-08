c9b12142c00345265b16334ccfb64a662ed578fe
Md Mojahid <aammojahid@gmail.com>
2024-11-08 15:48:59 +0600

    fixed nevigation

--- app/src/main/java/com/horizonhunters/tmdb/MainActivity.java
+++ app/src/main/java/com/horizonhunters/tmdb/MainActivity.java
@@ -2,17 +2,20 @@ package com.horizonhunters.tmdb;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.SearchView;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.horizonhunters.tmdb.discover.DiscoverFragment;
 import com.horizonhunters.tmdb.home.HomeFragment;
 import com.horizonhunters.tmdb.profile.ProfileFragment;
@@ -20,25 +23,30 @@ import com.horizonhunters.tmdb.profile.ProfileFragment;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize BottomNavigationView
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
-        if (savedInstanceState == null){
+        // Set default fragment on launch
+        if (savedInstanceState == null) {
             loadFragment(new HomeFragment());
         }
 
+        // Bottom navigation setup
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
-
                 int itemId = item.getItemId();
-                if (itemId == R.id.nav_home){
+                if (itemId == R.id.nav_home) {
                     fragment = new HomeFragment();
                 } else if (itemId == R.id.nav_discover) {
                     fragment = new DiscoverFragment();
@@ -47,18 +55,85 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     return false;
                 }
-                    if (fragment != null) {
-                        loadFragment(fragment);
-                    }
+                if (fragment != null) {
+                    loadFragment(fragment);
+                }
                 return true;
             }
         });
 
+        // Toolbar setup
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Drawer layout setup
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Navigation view setup (side drawer)
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Fragment fragment = null;
+                if (id == R.id.nav_home) {
+                    fragment = new HomeFragment();
+                } else if (id == R.id.nav_discover) {
+                    fragment = new DiscoverFragment();
+                } else if (id == R.id.nav_profile) {
+                    fragment = new ProfileFragment();
+                }
+                if (fragment != null) {
+                    loadFragment(fragment);
+                }
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        // SearchView setup
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle search query submission
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle text changes if needed
+                return false;
+            }
+        });
+
+        // Add animations to SearchView
+        addSearchViewAnimations();
     }
 
     private void loadFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.replace(R.id.FrameLayout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void addSearchViewAnimations() {
+        final Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        final Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    searchView.startAnimation(fadeIn);
+                } else {
+                    searchView.startAnimation(fadeOut);
+                }
+            }
+        });
     }
 }
